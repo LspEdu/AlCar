35f16ee8b824933dd5ea27c147e45e6cca8c63f2
Eduardo López <eduardo.lopez.manga@iesdonana.org>
2023-05-09 13:27:01 +0200

    Arreglando Registrar

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,9 +4,23 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Ape1 -->
+        <div class="mt-4">
+            <x-input-label for="ape1" :value="__('Primer Apellido')" />
+            <x-text-input id="ape1" class="block mt-1 w-full" type="text" name="ape1" :value="old('ape1')" required autofocus autocomplete="ape1" />
+            <x-input-error :messages="$errors->get('ape1')" class="mt-2" />
+        </div>
+
+        <!-- Ape2 -->
+        <div class="mt-4">
+            <x-input-label for="ape1" :value="__('Segundo Apellido')" />
+            <x-text-input id="ape1" class="block mt-1 w-full" type="text" name="ape1" :value="old('ape1')" required autofocus autocomplete="ape1" />
+            <x-input-error :messages="$errors->get('ape1')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
@@ -18,7 +32,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +44,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -41,11 +55,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('¿Ya tienes cuenta?') }}
             </a>
 
             <x-primary-button class="ml-4">
-                {{ __('Register') }}
+                {{ __('Registrar') }}
             </x-primary-button>
         </div>
     </form>

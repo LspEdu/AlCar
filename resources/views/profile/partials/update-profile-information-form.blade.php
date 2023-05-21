7c740419bbe80c48dc30fe743777a0d5512ba15b
Eduardo López <eduardo.lopez.manga@iesdonana.org>
2023-05-21 19:40:29 +0200

    Update de todas las propiedades de Usuario

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-            {{ __('Profile Information') }}
+            {{ __('Información Personal') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __("Update your account's profile information and email address.") }}
+            {{ __("Actualiza la información de tu cuenta") }}
         </p>
     </header>
 
@@ -18,9 +18,40 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nombre')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
+        </div>
+
+        <div>
+            <x-input-label for="ape1" :value="__('Apellido 1')" />
+            <x-text-input id="ape1" name="ape1" type="text" class="mt-1 block w-full" :value="old('ape1', $user->ape1)" required autofocus autocomplete="ape1" />
+            <x-input-error class="mt-2" :messages="$errors->get('ape1')" />
+        </div>
+
+        <div>
+            <x-input-label for="ape2" :value="__('Apellido 2')" />
+            <x-text-input id="ape2" name="ape2" type="text" class="mt-1 block w-full" :value="old('ape2', $user->ape2)"  autofocus autocomplete="ape2" />
+            <x-input-error class="mt-2" :messages="$errors->get('ape2')" />
+        </div>
+
+
+        <div>
+            <x-input-label for="fechNac" :value="__('Fecha Nacimiento')" />
+            <x-text-input id="fechNac" name="fechNac" type="date" class="mt-1 block w-full" :value="old('fechNac', $user->fechNac)" required autofocus autocomplete="fechNac" />
+            <x-input-error class="mt-2" :messages="$errors->get('fechNac')" />
+        </div>
+
+        <div>
+            <x-input-label for="direccion" :value="__('Dirección')" />
+            <x-text-input id="direccion" name="direccion" type="text" class="mt-1 block w-full" :value="old('direccion', $user->direccion)"  autofocus autocomplete="direccion" />
+            <x-input-error class="mt-2" :messages="$errors->get('direccion')" />
+        </div>
+
+        <div>
+            <x-input-label for="tlf" :value="__('Teléfono')" />
+            <x-text-input id="tlf" name="tlf" type="text" class="mt-1 block w-full" :value="old('tlf', $user->tlf)" required autofocus autocomplete="tlf" />
+            <x-input-error class="mt-2" :messages="$errors->get('tlf')" />
         </div>
 
         <div>
@@ -48,7 +79,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button>{{ __('Guardar') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
@@ -56,8 +87,8 @@
                     x-show="show"
                     x-transition
                     x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                    class="text-sm text-green-600 dark:text-green-400"
+                >{{ __('Guardado.') }}</p>
             @endif
         </div>
     </form>

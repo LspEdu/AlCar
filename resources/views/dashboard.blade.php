aefb46c245e079529ae61797acb16a3590f41444
Eduardo López <eduardo.lopez.manga@iesdonana.org>
2023-05-23 18:11:28 +0200

    Preparando para facturas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,94 +6,73 @@
         </h2>
     </x-slot>
 
-        <div class="row pt-2">
-          <div class="col-lg-4">
+    <div class="row pt-2">
+        <div class="col-lg-5">
             <div class="card mb-4 shadow">
-              <div class="card-body text-center row">
-                <img src="{{Auth::user()->avatar ?? asset('/storage/webo.jpg')}}" alt="avatar"
-                  class="rounded col-6" style="width: 150px;">
-                <h5 class="my-3 col-6">{{Auth::user()->name}} <br> {{Auth::user()->ape1}} {{Auth::user()->ape2}}</h5>
-              </div>
+                <div class="card-body text-center row">
+                    <img src="{{ Auth::user()->avatar ?? asset('/storage/webo.jpg') }}" alt="avatar"
+                        class="rounded col-6" style="width: 150px;">
+                    <h5 class="my-3 col-6">{{ Auth::user()->name }} <br> {{ Auth::user()->ape1 }}
+                        {{ Auth::user()->ape2 }}</h5>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Email</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <a href="mailto:{{Auth::user()->email}}" class="text-muted mb-0">{{ Auth::user()->email }}</a>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Teléfono</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">{{ Auth::user()->tlf }} </p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <p class="mb-0">Dirección</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <p class="text-muted mb-0">{{ Auth::user()->direccion ?? 'Desconocida' }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-          </div>
-          <div class="col-lg-8">
-            <div class="card mb-4 shadow">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-3">
-                    <p class="mb-0">Email</p>
-                  </div>
-                  <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{Auth::user()->email}}</p>
-                  </div>
-                </div>
-                <hr>
-                <div class="row">
-                  <div class="col-sm-3">
-                    <p class="mb-0">Teléfono</p>
-                  </div>
-                  <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{Auth::user()->tlf}} </p>
-                  </div>
-                </div>
-                <hr>
-                <div class="row">
-                  <div class="col-sm-3">
-                    <p class="mb-0">Dirección</p>
-                  </div>
-                  <div class="col-sm-9">
-                    <p class="text-muted mb-0">{{Auth::user()->direccion ?? 'Desconocida'}}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="card mb-4 shadow mb-md-0">
-                  <div class="card-body">
-                    <p class="mb-4"><span class="text-primary font-italic me-1">Coches</span>
-                    </p>
-                    @foreach (Auth::user()->coches as $coche )
-                        <a href="{{route('coche.show', ['id' => $coche->id])}}">{{$coche->marca}} {{$coche->modelo}} - {{$coche->matricula}}</a>
-                    @endforeach
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="card mb-4 shadow mb-md-0">
-                  <div class="card-body">
-                    <p class="mb-4"><span class="text-primary font-italic me-1">Comentarios</span>
-                    </p>
-                    <p class="mb-1" style="font-size: .77rem;">Web Design</p>
-                    <div class="progress rounded" style="height: 5px;">
-                      <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="mt-4 mb-1" style="font-size: .77rem;">Website Markup</p>
-                    <div class="progress rounded" style="height: 5px;">
-                      <div class="progress-bar" role="progressbar" style="width: 72%" aria-valuenow="72"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="mt-4 mb-1" style="font-size: .77rem;">One Page</p>
-                    <div class="progress rounded" style="height: 5px;">
-                      <div class="progress-bar" role="progressbar" style="width: 89%" aria-valuenow="89"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="mt-4 mb-1" style="font-size: .77rem;">Mobile Template</p>
-                    <div class="progress rounded" style="height: 5px;">
-                      <div class="progress-bar" role="progressbar" style="width: 55%" aria-valuenow="55"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
-                    <div class="progress rounded mb-2" style="height: 5px;">
-                      <div class="progress-bar" role="progressbar" style="width: 66%" aria-valuenow="66"
-                        aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
+
+        <div class="col-lg-7">
+            <div class="row gap-2">
+                <div class="col-12">
+                    <div class="card mb-4 shadow mb-md-0">
+                        <div class="card-body">
+                            <p class="mb-4"><span class="text-primary font-italic me-1">Coches</span>
+                            </p>
+                            <ul>
+                                @foreach (Auth::user()->coches as $coche)
+                                    <li><a class="link link-secondary"
+                                            href="{{ route('coche.show', ['id' => $coche->id]) }}">{{ $coche->marca }}
+                                            {{ $coche->modelo }} - {{ $coche->matricula }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="card mb-4 shadow mb-md-0">
+                        <div class="card-body">
+                            <p class="mb-4"><span class="text-primary font-italic me-1">Facturas</span>
+                            </p>
+                            <h4>¡No tienes facturas! ¿A qué esperas para alquilar?</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </x-app-layout>

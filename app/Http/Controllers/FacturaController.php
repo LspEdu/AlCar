772b3852aaa74bcda6f340d8437589410b74fbec
Eduardo López <eduardo.lopez.manga@iesdonana.org>
2023-05-25 20:10:02 +0200

    Facturas ahora recoge los días y los valida

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -26,19 +26,24 @@ class FacturaController extends Controller
     public function alquilar(Request $request, $id)
     {
         $coche = Coche::find($id);
+        $request->validate([
+            'fechaInicio' => 'required | date',
+            'fechaFin' => 'required | date',
+        ]);
 
+        $fechaInicio = new \DateTime($request->input('fechaInicio'));
+        $fechaFin = new \DateTime($request->input('fechaFin'));
+
+        $diff = $fechaFin->diff($fechaInicio);
         $factura = new Factura();
 
-        $hoy  = new DateTime('now');
-        $mañana = new DateTime('tomorrow');
 
-        //TODO:: Rellenar a través del formulario del request
-        $factura->FechaInicio = $hoy;
-        $factura->FechaFin = $mañana;
-        $factura->importe = $coche->precio*2;
+         $factura->FechaInicio = $fechaInicio;
+        $factura->FechaFin = $fechaFin;
+        $factura->importe = $coche->precio * $diff->days ;
         $factura->user()->associate($request->user());
         $factura->coche()->associate($coche);
-        $factura->codigo = $factura->FechaFin->format('Ymd').$factura->user->id.$factura->FechaInicio->format('Ymd').$coche->matricula;
+        $factura->codigo = $factura->FechaFin->format('Ymd').$factura->FechaInicio->format('Ymd').$coche->matricula;
         $factura->lat = $coche->lat;
         $factura->lng = $coche->lng;
         $factura->save();
